07e620678a90193ca9ee21abfd789a81c5c42b00
xylnx <xylnx@mailbox.org>
2022-11-04 09:30:02 +0100

    Improve drag and drop feature

--- dragAndDrop.js
+++ dragAndDrop.js
@@ -5,6 +5,13 @@
  * @see <a href="https://github.com/xylnx">https://github.com/xylnx</a>
  */
 const dragAndDrop = (function () {
+  /** Filters a nodeList for element nodes. Used to remove text nodes etc.
+   * @param {nodeList}
+   */
+  function filterForElementTypes(nodeList) {
+    return [...nodeList].filter((node) => node.nodeType == Node.ELEMENT_NODE);
+  }
+
   /**
    * Runs when a user starts dragging an item (ondragstart).
    * It sets the dataTransfer property to the id of the dragged element.
@@ -32,43 +39,114 @@ const dragAndDrop = (function () {
 
     // The index of the dragged element in the nodeList of all elements (will be set below)
     let dragindex = 0;
+    let dropindex = 0;
 
     // Create a clone of the element onto which the dragged element was dropped
     const clone = e.target.cloneNode(true);
     // Get ID of the dragged element => this ID was stored in drag() using `setData()`
     const data = e.dataTransfer.getData('text');
-    const curEl = document.getElementById(data);
+
+    // Dragged element
+    const curEl = document.getElementById(data); // delete this one
+    const dropee = document.getElementById(data);
+    // Drop zone element
+    const dragee = e.target;
+    console.log('HELLO', dropee.dataset.domPos);
 
     const cloneDomPos = e.target.dataset.domPos;
     const curDomPos = curEl.dataset.domPos;
 
     if (clone.id !== data) {
       // Get all list/list item elements
-      let nodeList = document.querySelector('.grid-container').childNodes;
-      for (let i = 0; i < nodeList.length; i++) {
-        if (nodeList[i].id === data) {
+      let children = document.querySelector('.grid-container').children;
+
+      // ToDo: Will this even be necessary?
+      for (let i = 0; i < children.length; i++) {
+        if (children[i].id === data) {
           dragindex = i;
+          console.log({ dragindex });
+        }
+        if (children[i].id === clone.id) {
+          dropindex = i;
+          console.log({ dropindex });
         }
       }
-      // Replace the node in the drop zone with the dragged node
-      document
-        .querySelector('.grid-container')
-        .replaceChild(document.getElementById(data), e.target);
 
-      // Insert the clone of the node in the drop zone at the previous index (dragindex) of the dragged node
-      document
-        .querySelector('.grid-container')
-        .insertBefore(
-          clone,
-          document.querySelector('.grid-container').childNodes[dragindex]
-        );
+      // Move an item down
+      if (dragindex < dropindex) {
+        console.log('%cmove item down', 'color: red');
+        // loop over all elements
+        for (i = 0; i < children.length; i++) {
+          let child = children[i];
+          if (
+            // decrement domPos if
+            // current domPOs <= dropindex
+            // current domPos > dragindex
+            Number(child.dataset.domPos) <= dropindex &&
+            Number(child.dataset.domPos > dragindex)
+          ) {
+            let newPos = String(Number(child.dataset.domPos) - 1);
+            console.log('newPos:', newPos);
+            model.updateDomPos({
+              id: child.id,
+              newPos,
+            });
+          }
+        }
+        let newPos = String(Number(dropindex) - 1);
+        model.updateDomPos({
+          id: dropee.id,
+          newPos: String(Number(dropindex) - 1),
+        });
+      }
 
-      // Model: get both elements and update their domPos val
-      model.updateDomPos({ id: curEl.id, newPos: cloneDomPos });
-      model.updateDomPos({ id: clone.id, newPos: curDomPos });
-      // View update data attrs of the swapped elements
-      clone.dataset.domPos = curDomPos;
-      curEl.dataset.domPos = cloneDomPos;
+      // Move an item up
+      if (dragindex > dropindex) {
+        console.log('%cmove item up', 'color: darkorange');
+        const dropeeDomPosStart = Number(dropee.dataset.domPos);
+
+        // loop over all elements
+        for (i = 0; i < children.length; i++) {
+          let child = children[i];
+          if (
+            // increment domPos by one if
+            // current domPos < dragindex (original position of the dragee)
+            // current domPos >= dropindex (element onto which we drop)
+            Number(child.dataset.domPos) < dragindex &&
+            Number(child.dataset.domPos >= dropindex)
+          ) {
+            let newPos = String(Number(child.dataset.domPos) + 1);
+            model.updateDomPos({
+              id: child.id,
+              newPos,
+            });
+          }
+        }
+      }
+
+      // Assign the new domPos to the dropee
+      // dragee becomes dropee
+      model.updateDomPos({ id: dropee.id, newPos: String(dropindex) });
+
+      // Rerender list overview or list
+      // TODO: Put this logic into controller.js
+
+      if (model.state.view === 'overview') {
+        view.renderLists({
+          lists: model.getLists(),
+          DOMStrings: controller.DOMStrings,
+        });
+      }
+
+      if (model.state.view === 'list') {
+        console.log(model.state.listID);
+        view.renderList({
+          list: model.getList(),
+          DOMStrings: controller.DOMStrings,
+        });
+      }
+
+      console.log('Lists after:', model.lists);
     }
   }
 
